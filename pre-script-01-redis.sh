b4be40f46a4e670b7885f6873b08b955e36c73a6
lehoatptit <90516963+lehoatptit@users.noreply.github.com>
2024-11-17 01:57:04 +0700

    Update pre-script-01-redis.sh

--- pre-script-01-redis.sh
+++ pre-script-01-redis.sh
@@ -14,10 +14,10 @@ sudo mkdir -p /opt/redis-master/redis-sentinel/conf/
 sudo curl -o /opt/redis-master/redis-sentinel/conf/sentinel.conf https://raw.githubusercontent.com/lehoatptit/redis-sentinel/main/sentinel.conf
 sudo mkdir -p /opt/redis-master/redis-sentinel/data
 sudo mkdir -p /opt/redis-master/redis-sentinel/log
-sudo curl -o  /opt/redis-master/redis-sentinel/Dockerfile https://raw.githubusercontent.com/lehoatptit/redis-sentinel/main/Dockerfile-redis-sentinel
-sudo curl -o  /opt/redis-master/redis-sentinel/sentinel-entrypoint.sh https://raw.githubusercontent.com/lehoatptit/redis-sentinel/main/sentinel-entrypoint.sh
-sudo curl -o  /opt/redis-master/redis-sentinel/docker-compose.yml https://raw.githubusercontent.com/lehoatptit/redis-sentinel/main/docker-compose-master-node.yml
-sudo curl -o  /opt/redis-master/redis-sentinel/.env ttps://raw.githubusercontent.com//lehoatptit/redis-sentinel/main/.env
+sudo curl -o /opt/redis-master/redis-sentinel/Dockerfile https://raw.githubusercontent.com/lehoatptit/redis-sentinel/main/Dockerfile-redis-sentinel
+sudo curl -o /opt/redis-master/redis-sentinel/sentinel-entrypoint.sh https://raw.githubusercontent.com/lehoatptit/redis-sentinel/main/sentinel-entrypoint.sh
+sudo curl -o /opt/redis-master/redis-sentinel/docker-compose.yml https://raw.githubusercontent.com/lehoatptit/redis-sentinel/main/docker-compose-master-node.yml
+sudo curl -o /opt/redis-master/redis-sentinel/.env ttps://raw.githubusercontent.com//lehoatptit/redis-sentinel/main/.env
 #change quyen thu thi
 sudo chmod -R 755 /opt/redis-master
 #hien thi danh sach cac thu muc /opt
