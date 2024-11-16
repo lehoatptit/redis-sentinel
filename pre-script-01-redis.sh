f5b8427c12fa51e9fee424f64eac2bf6fcacd92a
lehoatptit <90516963+lehoatptit@users.noreply.github.com>
2024-11-17 00:05:00 +0700

    Create pre-script-01-redis.sh

---
+++ pre-script-01-redis.sh
@@ -0,0 +1,21 @@
+#auto tao thu muc configure redis va redis sentinel
+cat << EOF > pre-script-01.sh
+#!/bin/bash
+# Install tree package
+dnf install tree -y 
+# tao thu muc va configure file redis.conf cho redis master node
+sudo mkdir -p /opt/redis-master/conf/
+sudo curl -o /opt/redis-master/conf/redis.conf https://raw.githubusercontent.com/lehoatptit/redis-sentinel/main/redis-sample.conf
+sudo mkdir -p /opt/redis-master/data
+sudo mkdir -p /opt/redis-master/log
+
+# tao thu muc va configure file sentinel.conf cho redis master node
+sudo mkdir -p /opt/redis-master/redis-sentinel/conf/
+sudo curl -o /opt/redis-master/redis-sentinel/conf/sentinel.conf https://raw.githubusercontent.com/lehoatptit/redis-sentinel/main/sentinel.conf
+sudo mkdir -p /opt/redis-master/redis-sentinel/data
+sudo mkdir -p /opt/redis-master/redis-sentinel/log
+
+#hien thi danh sach cac thu muc /opt
+echo " cay thu muc vua tao duoc la:"
+tree -L 3 /opt
+EOF
